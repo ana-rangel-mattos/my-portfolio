12abf63ab2c61f7dec9b6aee37fa9774e20b68dd
blastic.cato19 <anabrmattos@gmail.com>
2025-07-01 18:11:54 -0300

    Finished the hello page

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -5,7 +5,7 @@
 <template>
   <footer class="flex flex-col border-y-1 px-4 py-4 sm:flex-row sm:items-center sm:justify-between">
     <div class="flex h-full flex-row items-center gap-4">
-      <p class="font-medium text-slate-200">Find me in:</p>
+      <p class="font-medium text-slate-200">Me encontre no:</p>
 
       <a
         href="https://www.linkedin.com/in/ana-beatriz-rangel-mattos-7094b92b8?utm_source=share&utm_campaign=share_via&utm_content=profile"
@@ -22,7 +22,7 @@
 
     <div class="flex pt-2 sm:pt-0">
       <a href="https://github.com/ana-rangel-mattos" target="_blank" class="contents">
-        <p class="flex items-center justify-between pr-3 text-slate-100">@anabrmattos</p>
+        <p class="flex items-center justify-between pr-3 text-slate-100">@ana-rangel-mattos</p>
         <RiGithubFill class="icon text-slate-100" />
       </a>
     </div>
--- src/views/HelloView.vue
+++ src/views/HelloView.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <div>
-      <span>Olá, eu sou</span>
-      <h1>Ana Beatriz</h1>
-      <p>Full-Stack Devloper</p>
+  <main class="flex flex-col items-center gap-10 p-4 sm:justify-center sm:gap-14 sm:p-0">
+    <div class="sm:w-1/2">
+      <span class="text-lg sm:text-xl">Olá, eu sou</span>
+      <h1 class="text-5xl/normal text-slate-50 sm:text-6xl/normal">Ana Beatriz</h1>
+      <p class="text-2xl font-medium text-indigo-500 sm:text-3xl">> Full-Stack Developer</p>
     </div>
 
-    <div>
-      <span>// Find my profile on Github:</span>
+    <div class="px-4 sm:w-1/2 sm:px-0">
+      <span class="text-base/normal">// Meu perfil no GitHub:</span>
       <p>
-        <span>const</span> <span>githubLink</span> =
-        <span>https://github.com/ana-rangel-mattos</span>
+        <span class="font-medium text-indigo-500"
+          >const <span class="font-medium text-teal-400">githubLink</span></span
+        >
+        =
+        <span class="text-rose-300 underline decoration-rose-300"
+          >"<a class="contents" target="_blank" href="https://github.com/ana-rangel-mattos"
+            >https://github.com/ana-rangel-mattos</a
+          >"</span
+        >
       </p>
     </div>
   </main>
